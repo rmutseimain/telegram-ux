dadc4c3cf2663c78656d78fadd786c1dc28e30c9
rmutseimain <147353918+rmutseimain@users.noreply.github.com>
2023-12-30 03:23:53 +0200

    init

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,15 +1,12 @@
 import React, {useCallback, useEffect, useState} from 'react';
-import './Form.css'
+import './Form.css';
 import {useTelegram} from "../hooks/useTelegram";
-// const [countOfSleep, setCoundOfSleep ] = useState('');
-// const [qualityOfSleep, setQualityOfSleep ] = useState('');
-// const [energyLevel, setEnergyLevel ] = useState('');
-const Form = () => {
 
-    const [country, setCountry ] = useState('');
-    const [street, setStreet ] = useState('');
-    const [subject, setSubject ] = useState('physical');
-    const {tg} = useTelegram()
+const Form = () => {
+    const [country, setCountry] = useState('');
+    const [street, setStreet] = useState('');
+    const [subject, setSubject] = useState('physical');
+    const {tg} = useTelegram();
 
     const onSendData = useCallback(() => {
         const data = {
@@ -17,32 +14,29 @@ const Form = () => {
             street,
             subject
         }
-
         tg.sendData(JSON.stringify(data));
-    }, [tg, country, street, subject])
+    }, [country, street, subject])
 
     useEffect(() => {
-        tg.onEvent("mainButtonClicked", onSendData);
-
+        tg.onEvent('mainButtonClicked', onSendData)
         return () => {
-            tg.offEvent("mainButtonClicked", onSendData);
+            tg.offEvent('mainButtonClicked', onSendData)
         }
-    }, [tg, onSendData]);
+    }, [onSendData])
 
     useEffect(() => {
         tg.MainButton.setParams({
-            text: 'Отправить данние'
+            text: 'Отправить данные'
         })
-    }, [tg]);
-
+    }, [])
 
     useEffect(() => {
-        if (!street || !country) {
-            tg.MainButton.hide()
+        if(!street || !country) {
+            tg.MainButton.hide();
         } else {
-            tg.MainButton.show()
+            tg.MainButton.show();
         }
-    }, [tg, country, street]);
+    }, [country, street])
 
     const onChangeCountry = (e) => {
         setCountry(e.target.value)
@@ -57,27 +51,22 @@ const Form = () => {
     }
 
     return (
-        <div className={'form'}>
-            <h3> Введите данные </h3>
+        <div className={"form"}>
+            <h3>Введите ваши данные</h3>
             <input
                 className={'input'}
                 type="text"
-                placeholder={"Страна"}
+                placeholder={'Страна'}
                 value={country}
                 onChange={onChangeCountry}
             />
-
             <input
                 className={'input'}
                 type="text"
-                placeholder={"Улица"}
+                placeholder={'Улица'}
                 value={street}
                 onChange={onChangeStreet}
             />
-            {/*<input className={'input'} type="text" placeholder={"Количестно часов сна"}/>*/}
-            {/*<input className={'input'} type="text" placeholder={"Качество сна"}/>*/}
-            {/*<input className={'input'} type="text" placeholder={"Уровень енергии"}/>*/}
-
             <select value={subject} onChange={onChangeSubject} className={'select'}>
                 <option value={'physical'}>Физ. лицо</option>
                 <option value={'legal'}>Юр. лицо</option>
