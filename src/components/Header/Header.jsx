20db1b7f22e9e3d23a008f5d6ad1edd3ee0c07a5
rmutseimain <147353918+rmutseimain@users.noreply.github.com>
2024-01-25 00:25:03 +0200

    reworking

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -23,35 +23,35 @@ const Header = () => {
         //     </ul>
         // </div>
 
-        // <div className="dropdown">
-        //     <a className="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="true">
-        //         Dropdown link
-        //     </a>
-        //
-        //     <ul className="dropdown-menu">
-        //         <li><a className="dropdown-item" href="#" onClick={() => setState('Home')}>Home</a></li>
-        //             <li><a className="dropdown-item" href="#" onClick={() => setState('Morning')}>Morning survey</a></li>
-        //         <li><a className="dropdown-item" href="#" onClick={() => setState('Evening')}>Eventing survey</a></li>
-        //     </ul>
-        // </div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon">asdddd</span>sss
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul className="navbar-nav">
-                    <li className="nav-item active">
-                        <a className="nav-link" onClick={() => setState('Home')} >Home <span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" onClick={() => setState('Morning')}>Morning survey</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" onClick={() => setState('Evening')}>Eventing survey</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+        <div className="dropdown">
+            <a className="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="true">
+                Dropdown link
+            </a>
+
+            <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#" onClick={() => setState('Home')}>Home</a></li>
+                    <li><a className="dropdown-item" href="#" onClick={() => setState('Morning')}>Morning survey</a></li>
+                <li><a className="dropdown-item" href="#" onClick={() => setState('Evening')}>Eventing survey</a></li>
+            </ul>
+        </div>
+        // <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        //     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        //         <span className="navbar-toggler-icon">asdddd</span>sss
+        //     </button>
+        //     <div className="collapse navbar-collapse" id="navbarNavDropdown">
+        //         <ul className="navbar-nav">
+        //             <li className="nav-item active">
+        //                 <a className="nav-link" onClick={() => setState('Home')} >Home <span className="sr-only">(current)</span></a>
+        //             </li>
+        //             <li className="nav-item">
+        //                 <a className="nav-link" onClick={() => setState('Morning')}>Morning survey</a>
+        //             </li>
+        //             <li className="nav-item">
+        //                 <a className="nav-link" onClick={() => setState('Evening')}>Eventing survey</a>
+        //             </li>
+        //         </ul>
+        //     </div>
+        // </nav>
     )
 };
 
