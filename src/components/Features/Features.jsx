6ed82282b63ef09afe15937f2c9f8dbdee5c3f6b
rmutseimain <147353918+rmutseimain@users.noreply.github.com>
2024-01-26 01:14:16 +0200

    change features sizing

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -5,9 +5,11 @@ function Features(props) {
     return (
         <div className="input-group mb-3">
             <label className="input-group-text" htmlFor="inputGroupSelect01" style={{
-                width: '26%'
+                width: '80%'
             }}>{props.question.id}. {props.question.name}</label>
-            <select className="form-select" id="inputGroupSelect01" >
+            <select className="form-select" id="inputGroupSelect01" style={{
+                width: '20%'
+            }}>
                 <option selected>Choose...</option>
                 <option value="1">1</option>
                 <option value="2">2</option>
